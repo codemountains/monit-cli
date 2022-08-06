9d3a1372cdf2c247ee01c0ea8c846d8348787b18
codemountains <4kz12zz@gmail.com>
2022-08-07 02:07:38 +0900

    :sparkles: monit-cli

---
+++ src/main.rs
@@ -0,0 +1,115 @@
+use std::process;
+use ansi_term::Colour;
+use chrono::{DateTime, Local};
+use clap::{ArgEnum, Parser};
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+use reqwest::StatusCode;
+
+#[derive(Parser, Debug)]
+#[clap(name = "monit", version, about, long_about = None, arg_required_else_help = true)]
+struct Args {
+    /// URL to be monitored for response time
+    #[clap(value_parser)]
+    url: String,
+
+    /// Interval seconds
+    #[clap(
+        short,
+        long,
+        value_parser,
+        value_name = "INTERVAL SECONDS",
+        default_value_t = 30
+    )]
+    interval: u64,
+
+    /// Output type
+    #[clap(short, long, arg_enum, value_name = "OUTPUT TYPE", default_value = "text")]
+    output: Output
+}
+
+#[derive(Debug, Clone, ArgEnum, Copy)]
+enum Output {
+    Csv,
+    Json,
+    Text,
+}
+
+struct OutputMessage {
+    datetime: DateTime<Local>,
+    url: String,
+    status: StatusCode,
+    elapsed: Duration,
+}
+
+impl OutputMessage {
+    fn new(datetime: DateTime<Local>, url: String, status: StatusCode, elapsed: Duration) -> Self {
+        Self {
+            datetime,
+            url,
+            status,
+            elapsed
+        }
+    }
+
+    fn to_formatted(&self, output: Output) -> String {
+        let dt = self.datetime.format("%Y-%m-%d %H:%M:%S").to_string();
+        let url = self.url.as_str().to_string();
+        let st = self.status.to_string();
+        let response_time = format!("{}.{:03}", self.elapsed.as_secs(), self.elapsed.subsec_nanos() / 1_000_000);
+
+        match output {
+            Output::Csv => {
+                format!(r#""{}","{}","{}","{}""#, dt, url, st, response_time)
+            },
+            Output::Json => {
+                format!(r#"{{"datetime": "{}","url: "{}", "status": "{}","responseTime": "{}"}}"#, dt, url, st, response_time)
+            },
+            Output::Text => {
+                format!("{} {} {} {}", dt, url, st, response_time)
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // comment in on Windows 10
+    // let _ = ansi_term::enable_ansi_support();
+
+    let args = Args::parse();
+    let url: &str = &args.url;
+    let interval: u64 = args.interval;
+    let output: Output = args.output;
+
+    loop {
+        let dt_now = Local::now();
+
+        let start = Instant::now();
+        let resp = reqwest::get(url).await;
+        let end = start.elapsed();
+
+        match resp {
+            Ok(resp) => {
+                let status = resp.status();
+
+                let output_msg = OutputMessage::new(dt_now, url.to_string(), status, end);
+                let msg = output_msg.to_formatted(output);
+
+                let color_msg = if status.is_success() {
+                    Colour::Green.paint(&msg)
+                } else {
+                    Colour::Red.paint(&msg)
+                };
+
+                println!("{}", color_msg);
+            }
+            Err(err) => {
+                eprintln!("{}", err);
+                process::exit(1);
+            }
+        }
+
+        sleep(Duration::from_secs(interval));
+    }
+}
